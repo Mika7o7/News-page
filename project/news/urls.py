29baf55c6d1006ebba6e1176a1bd98b43a80ebbe
Mika7o7 <mikaohanyan881@gmail.com>
2022-07-11 00:45:44 +0000

    add register, login, logout, password_reset

--- project/news/urls.py
+++ project/news/urls.py
@@ -1,11 +1,51 @@
 from django.urls import path
-from .views import *
+from . import views
+
 
 urlpatterns = [
-    path('', HomeNews.as_view(), name='home'),
-    path('create', add_news, name='create'),
-    path('add_category', add_category, name='add_category'),
-    path('news_ditail/<int:pk>', news_ditail, name='news_ditail'),
-    path('category/<int:category_id>', NewsByCategory.as_view(),
-         name='category'),
+    path(
+        '',
+        views.HomeNews.as_view(),
+        name='home',
+    ),
+    path(
+        'create',
+        views.add_news,
+        name='create',
+    ),
+    path(
+        'add_category',
+        views.add_category,
+        name='add_category',
+    ),
+    path(
+        'news_ditail/<int:pk>',
+        views.news_ditail,
+        name='news_ditail',
+    ),
+    path(
+        'category/<int:category_id>',
+        views.NewsByCategory.as_view(),
+        name='category',
+    ),
+    path(
+        "register",
+        views.register_request,
+        name="register",
+    ),
+    path(
+        "login",
+        views.login_request,
+        name="login",
+    ),
+    path(
+        "logout",
+        views.logout_request,
+        name="logout",
+    ),
+    path(
+        "password_reset",
+        views.password_reset_request,
+        name="password_reset",
+    ),
 ]
